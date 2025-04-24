bf62181c8022712368106d3795ab1cc50289181e
sandwichman3 <yinamy@outlook.com>
2025-04-24 17:04:16 +0100

    !!! This version works with resource predicate support

--- backend/cn/lib/list_rev/theories/Gen_Spec.v
+++ backend/cn/lib/list_rev/theories/Gen_Spec.v
@@ -2,6 +2,7 @@
 
 Require Import ZArith Bool.
 Require CN_Lemmas.CN_Lib.
+Require Import CN_Lemmas.CN_Lib_Iris.
 
 
 Module Types.
@@ -16,16 +17,29 @@ End Types.
 
 Module Type Parameters.
   Import Types.
+  Open Scope Z.
+  Parameter Alloc : (Z * Z) -> Prop.
 
-  (* no parameters required *)
 
 End Parameters.
 
 
 Module Defs (P : Parameters).
-
+  (* Definitions of functions, structs, and struct ownership predicates *)
   Import Types P.
   Open Scope Z.
+
+  (* Opening Iris mode *)
+  Section Defs.
+  Context `{!heapGS_gen Σ}.
+
+  Record sllist : Type := { 
+  head : Z; 
+
+  tail : Ptr; 
+ }.
+
+  Definition Owned_sllist (l: Ptr) (v : sllist) : iProp Σ := Owned_int (CN_Lib_Iris.shift l 0 4) v.(head) ∗ padding (arrayshift l 4 1) 4 ∗ Owned_int (CN_Lib_Iris.shift l 8 8) v.(tail).
 
   Definition Hd (L : List) :=
     (match L with
@@ -52,28 +66,78 @@ Module Defs (P : Parameters).
 
  | (Nil) => (Nil) | (Cons H T) => (Snoc (RevList T) H) end).
 
-  Definition Append_Nil_RList_type : Prop :=
-    forall (L1 : List),
-(Is_true true) -> ((Append L1 (Nil)) = L1) /\ Is_true true.
 
-  Definition Append_Cons_RList_type : Prop :=
-    forall (L1 : List),
-forall (X : Z),
-forall (L2 : List),
-(Is_true true) -> ((Append L1 (Cons X L2)) = (Append (Snoc L1 X) L2)) /\ Is_true true.
+  (* Closing Iris mode *)
+  End Defs.
 
 End Defs.
 
 
+Module ResourcePredicates (P : Parameters).
+  Module D := Defs(P).
+  Import Types P D.
+  Open Scope Z.
+  Unset Guard Checking.
+  (* Opening Iris mode *)
+  Section Iris_Pred_Defs.
+  Context `{!heapGS_gen Σ}.
+
+  Fixpoint SLList_At (p : Ptr)  (ret : List) {struct ret}  : iProp Σ := 
+(⌜ (p = 0) ⌝  ∧  ⌜ ret = (Nil) ⌝) ∨ (⌜ (Is_true true) ⌝ ∧ ⌜ ~(p = 0) ⌝  ∧ 
+∃ (H : sllist),
+Owned_sllist p H  ∗ ∃ (T : List), SLList_At H.(tail) T ∗ ⌜ ret = (Cons H.(head) T) ⌝).
+
+
+  (* Closing Iris mode *)
+  End Iris_Pred_Defs.
+End ResourcePredicates.
+
+
+Module Lemma_Defs (P : Parameters).
+  Module D := Defs(P).
+  Module R := ResourcePredicates(P).
+  Import Types D P R.
+  Open Scope Z.
+
+
+
+  (* Opening Iris mode *)
+  Section Iris_Type_Defs.
+  Context `{!heapGS_gen Σ}.
+
+  Definition Append_Nil_RList_type : iProp Σ :=
+    ∀ (L1 : List),
+⌜ (Is_true true) ⌝ -∗ ⌜ ((Append L1 (Nil)) = L1) ⌝ ∧ ⌜ Is_true true ⌝.
+
+  Definition Append_Cons_RList_type : iProp Σ :=
+    ∀ (L1 : List),
+∀ (X : Z),
+∀ (L2 : List),
+⌜ (Is_true true) ⌝ -∗ ⌜ ((Append L1 (Cons X L2)) = (Append (Snoc L1 X) L2)) ⌝ ∧ ⌜ Is_true true ⌝.
+
+
+  (* Closing Iris mode *)
+  End Iris_Type_Defs.
+End Lemma_Defs.
+
+
 Module Type Lemma_Spec (P : Parameters).
 
-  Module D := Defs(P).
-  Import D.
+  Module L := Lemma_Defs(P).
+  Import L.
+  (* Opening Iris mode *)
+  Section Lemma_Defs.
+  Context `{!heapGS_gen Σ}.
 
-  Parameter Append_Nil_RList : Append_Nil_RList_type.
+  Local Notation "⊢ P" := (⊢@{iPropI Σ} P).
 
-  Parameter Append_Cons_RList : Append_Cons_RList_type.
+  Parameter Append_Nil_RList : ⊢ Append_Nil_RList_type.
 
+  Parameter Append_Cons_RList : ⊢ Append_Cons_RList_type.
+
+
+  (* Closing Iris mode *)
+  End Lemma_Defs.
 End Lemma_Spec.
 
 
--- backend/cn/lib/list_rev/theories/Inst_Spec.v
+++ backend/cn/lib/list_rev/theories/Inst_Spec.v
@@ -3,24 +3,36 @@
 
 Require Import ZArith Bool Lia.
 Require Import CN_Lemmas.Gen_Spec.
+Require CN_Lemmas.CN_Lib.
+Require Import CN_Lemmas.CN_Lib_Iris.
 Import CN_Lemmas.Gen_Spec.Types.
 Require Import List.
 Import ListNotations.
+From iris.proofmode Require Import proofmode.
+
 
 Module Inst.
 (* nothing!! *)
+Definition Alloc : (Z * Z) -> Prop := fun _ => True.
+
 End Inst.
 
-Module Defs := CN_Lemmas.Gen_Spec.Defs (Inst).
+Module Lemma_Defs := CN_Lemmas.Gen_Spec.Lemma_Defs (Inst).
 
 Module Proofs.
 
 (* now prove lemmas *)
-Import Defs.
+Import Lemma_Defs Inst.
 Open Scope Z.
 
-Lemma Append_Nil_RList : Append_Nil_RList_type.
+Section Lemma_Defs.
+Context `{!heapGS_gen Σ}.
+
+Local Notation "⊢ P" := (⊢@{iPropI Σ} P).
+
+Lemma Append_Nil_RList : ⊢ Append_Nil_RList_type.
 Proof.
+  iPureIntro.
   intros L1 _. 
   induction L1.
   + split; reflexivity.
@@ -28,20 +40,21 @@ Proof.
     rewrite H; auto.
 Qed.
 
-Lemma Append_Cons_RList : Append_Cons_RList_type.
+Lemma Append_Cons_RList : ⊢ Append_Cons_RList_type.
 Proof.
+  iPureIntro.
   intros L1 z L2 t.
   induction L1; auto.
   simpl. destruct IHL1. split; auto.
   rewrite H; auto.
 Qed.
 
-
+End Lemma_Defs.
 End Proofs.
 
 Module InstOK: CN_Lemmas.Gen_Spec.Lemma_Spec(Inst).
 
-  Module D := CN_Lemmas.Gen_Spec.Defs (Inst).
+  Module L := CN_Lemmas.Gen_Spec.Lemma_Defs (Inst).
 
   Include Proofs.
 
